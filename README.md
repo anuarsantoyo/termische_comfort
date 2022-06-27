bc400cecd20c667b9ff472881de6567a81a59d2b
Anuar Santoyo <anuar.santoyoalum@campus.tu-berlin.de>
2022-06-27 14:49:34 +0200

    implemented NNClassifier

--- README.md
+++ README.md
@@ -3,14 +3,14 @@ Goal: Test several models that predict thermal comfort based on Feature vector F
 
 # Approaches:
 ## Predictor: F -> [-3, 3] 
-- Linear
-- NN
+- Linear (implemented)
+- NN (implemented)
 - Fanger
 - Hybrid: From Fanger calculate W using NN or linear model
 - Logistic
 
 ## Clasifier: F -> {-1, 0, 1}
-- NN
+- NN (implemented)
 - Random Forest
 - Adaboost
 - SVM
@@ -25,3 +25,6 @@ new data
 ## Mean Votation Polyfit:
 Use rolling discrete hypercube to calculate mean comfort (MC) within hypercube on discrete points throughout feature 
 space V. Fit polynomial (or kernel approach) so that it predicts F -> MC 
+ todo:
+- ver paper relacionados a buses
+- 
--- methods.py
+++ methods.py
@@ -10,10 +10,7 @@ from sklearn import svm
 import random
 import numpy as np
 
-
-
-
-
+# Networks
 
 class Net(nn.Module):
     def __init__(self, device=None, dtype=None, input_size=1):
@@ -33,9 +30,65 @@ class Net(nn.Module):
         return F.relu(x)
 
 
-class NN:
+class NetClasifier(nn.Module):
+    def __init__(self, device=None, dtype=None, input_size=1):
+        self.device = device
+        self.dtype = dtype
+        super(NetClasifier, self).__init__()
+        self.fc1 = nn.Linear(input_size, input_size, device=self.device, dtype=self.dtype)
+        self.fc2 = nn.Linear(input_size, 6, device=self.device, dtype=self.dtype)
+        self.fc3 = nn.Linear(6, 3, device=self.device, dtype=self.dtype)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class LinearNet(nn.Module):
+    def __init__(self, device=None, dtype=None, input_size=1):
+        self.device = device
+        self.dtype = dtype
+        super(LinearNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, 1, device=self.device, dtype=self.dtype)
+
+    def forward(self, x):
+        x = self.fc1(x.float())
+        return x  # torch.sigmoid(x) #torch.tanh(x*3-1.5) + 1 #
+
+####################################################################################################################
+
+# Methods
+
+
+class NNClassifier:
     def __init__(self, n_observations=None, device=None, dtype=None, input_size=1):
-        self.optimizes = True
+        self.criterion = nn.CrossEntropyLoss()
+        self.type = "classifier"
+        self.input_size = input_size
+        self.device = device
+        self.dtype = dtype
+        self.n_observations = n_observations
+        self.model = NetClasifier(device=self.device, dtype=self.dtype, input_size=self.input_size)
+
+    def get_parameters(self):
+        return list(set(self.model.parameters()))
+
+    def predict(self, x):
+        return self.model(x)
+
+    def calculate_regularization_loss(self):
+        reg_loss = 0
+        for param in self.model.parameters():
+            reg_loss += param.norm(2) ** 2
+        return reg_loss
+
+
+
+class NNPredictor:
+    def __init__(self, n_observations=None, device=None, dtype=None, input_size=1):
+        self.criterion = nn.MSELoss()
+        self.type = "predictor"
         self.input_size = input_size
         self.device = device
         self.dtype = dtype
@@ -55,21 +108,10 @@ class NN:
         return reg_loss
 
 
-class LinearNet(nn.Module):
-    def __init__(self, device=None, dtype=None, input_size=1):
-        self.device = device
-        self.dtype = dtype
-        super(LinearNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, 1, device=self.device, dtype=self.dtype)
-
-    def forward(self, x):
-        x = self.fc1(x.float())
-        return x  # torch.sigmoid(x) #torch.tanh(x*3-1.5) + 1 #
-
-
 class Linear:
     def __init__(self, n_observations=None, device=None, dtype=None, input_size=1):
-        self.optimizes = True
+        self.criterion = nn.MSELoss()
+        self.type = "predictor"
         self.input_size = input_size
         self.device = device
         self.dtype = dtype
@@ -84,6 +126,7 @@ class Linear:
 
     def calculate_regularization_loss(self):
         return torch.tensor(0.0, device=self.device, dtype=self.dtype)
+
 
 class SVM:
     def __init__(self, device=None, dtype=None, input_size=1):
